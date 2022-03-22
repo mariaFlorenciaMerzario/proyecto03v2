fe2aee341a887933afc1df63bec15c5191ad90d2
maria merzario <maria.merzario@davinci.edu.ar>
2022-03-22 08:32:22 -0300

    2 modificacion

--- api/usuarios.php
+++ api/usuarios.php
@@ -4,19 +4,7 @@ require_once 'bootstrap/init.php';
 
 switch($_SERVER['REQUEST_METHOD']) {
     case 'GET':
-        if(isset($_GET['id'])) {
-            $usuario = new Usuario;
-            $usuario = $usuario->usuariosTraerPorPk($_GET['id']);
-            echo json_encode([
-                'data' => $usuario
-            ]);
-        } else {
-            $usuario = new Usuario;
-            $usuario = $usuario->usuariosTodos($db);
-            echo json_encode([
-                'data' => $usuario
-            ]);
-        }
+       
         break;
 
     case 'POST':
@@ -39,44 +27,14 @@ switch($_SERVER['REQUEST_METHOD']) {
         break;
 
     case 'PUT':
-        $id = ($_GET['id']);
-        $jsonData = file_get_contents('php://input');
-        $putData = json_decode($jsonData, true);
+       
 
-        $validator = new \Validation\Validator($putData, [
-            'nombre'            => ['required', 'min:3'],
-            'password'          => ['required', 'min:3'],
-            'email'             => ['required', 'min:5'],
-           
-        ]);
-        if (!$validator->passes()) {
-            echo json_encode([
-                'success' => false,
-                'errores' => $validator->getErrores()
-            ]);
-            exit;
-        }
-
-        $usuario = new Usuario;
-        $exito = $usuario->usuariosEditar($id, $putData);
-        echo mysqli_error($db);
-        echo json_encode([
-            'success' => $exito
-        ]);
         break;
 
     case 'PATCH':
         break;
 
-    case 'DELETE':
-        $id = $_GET['id'];
-        $usuario = new Usuario;
-        $exito = $usuario->usuariosEliminar($db, $id);
-        echo json_encode([
-            'success' => $exito
-        ]);
-        break;
-
+    
     default:
         die('No existe la acción que estás buscando >:(');
 }
